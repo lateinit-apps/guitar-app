bb810925a96f98aac2c227b47b4ea7ca00dabfc7
kostmetallist <kostmetallist@gmail.com>
2020-04-06 01:44:18 +0300

    Adjust db config file format

--- app/model/connection.py
+++ app/model/connection.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
 from misc import Base
 from crack import *
-from db_config import db_dialect, db_driver, db_username, db_password, db_host, db_port, db_name
+import db_config
 
 
-if db_driver:
-    db_url = f"{db_dialect}+{db_driver}://{db_username}:" \
-             + f"{db_password}@{db_host}:{db_port}/{db_name}"
+if db_config.driver:
+    db_url = f"{db_config.dialect}+{db_config.driver}://{db_config.username}:" \
+             + f"{db_config.password}@{db_config.host}:{db_config.port}/{db_config.name}"
 else:
-    db_url = f"{db_dialect}://{db_username}:" \
-             + f"{db_password}@{db_host}:{db_port}/{db_name}"
+    db_url = f"{db_config.dialect}://{db_config.username}:" \
+             + f"{db_config.password}@{db_config.host}:{db_config.port}/{db_config.name}"
 
 engine = create_engine(db_url)
 Base.metadata.bind = engine
--- samples/db_config.py
+++ samples/db_config.py
@@ -1,7 +1,7 @@
-db_dialect = 'postgresql'
-db_driver = 'pg8000'
-db_username = 'crack_user'
-db_password = 'XXXXXXXXXX'
-db_host = 'localhost'
-db_port = 5432
-db_name = 'crack'
+dialect = 'postgresql'
+driver = 'pg8000'
+username = 'crack_user'
+password = 'XXXXXXXXXX'
+host = 'localhost'
+port = 5432
+name = 'crack'
