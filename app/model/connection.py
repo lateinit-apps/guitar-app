c6718b5d32ab1ca8dc97696d26f6da500fe3abb6
kostmetallist <kostmetallist@gmail.com>
2020-04-07 05:02:24 +0300

    Make strings formatting homogeneous

--- app/model/connection.py
+++ app/model/connection.py
@@ -5,11 +5,11 @@ import db_config
 
 
 if db_config.driver:
-    db_url = f"{db_config.dialect}+{db_config.driver}://{db_config.username}:" \
-             + f"{db_config.password}@{db_config.host}:{db_config.port}/{db_config.name}"
+    db_url = f'{db_config.dialect}+{db_config.driver}://{db_config.username}:' \
+             + f'{db_config.password}@{db_config.host}:{db_config.port}/{db_config.name}'
 else:
-    db_url = f"{db_config.dialect}://{db_config.username}:" \
-             + f"{db_config.password}@{db_config.host}:{db_config.port}/{db_config.name}"
+    db_url = f'{db_config.dialect}://{db_config.username}:' \
+             + f'{db_config.password}@{db_config.host}:{db_config.port}/{db_config.name}'
 
 engine = create_engine(db_url)
 Base.metadata.bind = engine
@@ -23,11 +23,11 @@ def drop_tables():
     Base.metadata.drop_all()
 
 
-if __name__ == "__main__":
-    actions = ["create", "drop"]
-    print("possible actions: ", actions)
-    choice = input("> ")
-    if choice == "create":
+if __name__ == '__main__':
+    actions = ['create', 'drop']
+    print('possible actions: ', actions)
+    choice = input('> ')
+    if choice == 'create':
         create_tables()
-    elif choice == "drop":
+    elif choice == 'drop':
         drop_tables()
--- app/model/crack.py
+++ app/model/crack.py
@@ -4,18 +4,18 @@ from misc import Base
 
 
 class Genre(Base):
-    __tablename__ = "genre"
+    __tablename__ = 'genre'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
     highlights = Column(Text)
 
-    artists = relationship("Artist", secondary="genre_artist")
-    releases = relationship("Release", secondary="genre_release")
+    artists = relationship('Artist', secondary='genre_artist')
+    releases = relationship('Release', secondary='genre_release')
 
 
 class Artist(Base):
-    __tablename__ = "artist"
+    __tablename__ = 'artist'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
@@ -23,19 +23,19 @@ class Artist(Base):
     country = Column(String(32))
     about = Column(Text)
 
-    genres = relationship(Genre, secondary="genre_artist")
-    releases = relationship("Release", secondary="artist_release")
+    genres = relationship(Genre, secondary='genre_artist')
+    releases = relationship('Release', secondary='artist_release')
 
 
 class GenreArtist(Base):
-    __tablename__ = "genre_artist"
+    __tablename__ = 'genre_artist'
 
     genre_id = Column(Integer, ForeignKey(Genre.id), primary_key=True)
     artist_id = Column(Integer, ForeignKey(Artist.id), primary_key=True)
 
 
 class Release(Base):
-    __tablename__ = "release"
+    __tablename__ = 'release'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
@@ -43,58 +43,58 @@ class Release(Base):
     label = Column(String(64))
     type = Column(String(32))
     album_kind = Column(String(32))
-    release_id = Column(Integer, ForeignKey("release.id"))
+    release_id = Column(Integer, ForeignKey('release.id'))
 
-    genres = relationship(Genre, secondary="genre_release")
-    artists = relationship(Artist, secondary="artist_release")
-    songs = relationship("Song", secondary="release_song")
-    embracing_release = relationship("Release", remote_side=[id], backref="included_release")
+    genres = relationship(Genre, secondary='genre_release')
+    artists = relationship(Artist, secondary='artist_release')
+    songs = relationship('Song', secondary='release_song')
+    embracing_release = relationship('Release', remote_side=[id], backref='included_release')
 
 
 class GenreRelease(Base):
-    __tablename__ = "genre_release"
+    __tablename__ = 'genre_release'
 
     genre_id = Column(Integer, ForeignKey(Genre.id), primary_key=True)
     release_id = Column(Integer, ForeignKey(Release.id), primary_key=True)
 
 
 class ArtistRelease(Base):
-    __tablename__ = "artist_release"
+    __tablename__ = 'artist_release'
 
     artist_id = Column(Integer, ForeignKey(Artist.id), primary_key=True)
     release_id = Column(Integer, ForeignKey(Release.id), primary_key=True)
 
 
 class Song(Base):
-    __tablename__ = "song"
+    __tablename__ = 'song'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(64))
     trivia = Column(Text)
 
-    releases = relationship(Release, secondary="release_song")
+    releases = relationship(Release, secondary='release_song')
 
 
 class ReleaseSong(Base):
-    __tablename__ = "release_song"
+    __tablename__ = 'release_song'
 
     release_id = Column(Integer, ForeignKey(Release.id), primary_key=True)
     song_id = Column(Integer, ForeignKey(Song.id), primary_key=True)
 
 
 class Sheet(Base):
-    __tablename__ = "sheet"
+    __tablename__ = 'sheet'
 
     id = Column(Integer, primary_key=True)
     date_uploaded = Column(Date)
     bpm = Column(Integer)
     song_id = Column(Integer, ForeignKey(Song.id))
 
-    song = relationship(Song, backref="sheets")
+    song = relationship(Song, backref='sheets')
 
 
 class TrackTab(Base):
-    __tablename__ = "tracktab"
+    __tablename__ = 'tracktab'
 
     id = Column(Integer, primary_key=True)
     instrument = Column(String(32))
@@ -103,4 +103,4 @@ class TrackTab(Base):
     gp5 = Column(LargeBinary)
     sheet_id = Column(Integer, ForeignKey(Sheet.id))
 
-    sheet = relationship(Sheet, backref="tracktabs")
+    sheet = relationship(Sheet, backref='tracktabs')
