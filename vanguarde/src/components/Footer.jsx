4bdc0eca9c43b07814b47f02b469d46bdc52b3b1
xelav <xelav.rus@gmail.com>
2020-04-25 17:15:04 +0300

    Very secret sauce

--- vanguarde/src/components/Footer.jsx
+++ vanguarde/src/components/Footer.jsx
@@ -3,12 +3,49 @@ import {MDBContainer, MDBFooter} from "mdbreact";
 
 export class Footer extends Component {
 
+    constructor(props){
+        super(props);
+
+
+        this.footerClicks = 0;
+        this.footerArray = [
+            '© ' + new Date().getFullYear() + ' Copyright LateInit Apps',
+            'Шёл медведь по лесу, видит - машина горит. Сел в неё и сгорел',
+            'Copyright © 2007. All bears reserved.',
+            'Надел мужик шляпу.',
+            'А она ему как раз.'
+        ];
+
+        this.state = {
+            footerText: this.footerArray[0]
+        };
+
+        this.handleFooterClick = this.handleFooterClick.bind(this);
+
+    }
+
+    handleFooterClick(e){
+        this.footerClicks += 1;
+        if (this.footerClicks > 5){
+            let newFooterText = this.footerArray[(this.footerClicks - 5) % this.footerArray.length];
+            this.setState({footerText: newFooterText});
+        }
+    }
+
     render() {
         return (
-            <MDBFooter color="elegant-color" className="font-small pt-4 mt-4 fixed-bottom">
+            <MDBFooter color="elegant-color" className="font-small pt-4 mt-4 fixed-bottom" onClick={this.handleFooterClick}
+                       style={{
+                           WebkitTouchCallout: 'none',
+                           WebkitUserSelect: 'none',
+                           KhtmlUserSelect: 'none',
+                           MozUserSelect: 'none',
+                           MsUserSelect: 'none',
+                           userSelect: 'none',
+                       }}>
                 <div className="footer-copyright text-center py-3">
-                    <MDBContainer fluid>
-                        &copy; {new Date().getFullYear()} Copyright LateInit Apps
+                    <MDBContainer fluid >
+                        {this.state.footerText}
                     </MDBContainer>
                 </div>
             </MDBFooter>
