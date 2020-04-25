d6778e5ea80e0b4c715ecb944244e6eced7738a0
xelav <xelav.rus@gmail.com>
2020-04-25 16:40:08 +0300

    Fix padding issues

--- vanguarde/src/components/SongCard.jsx
+++ vanguarde/src/components/SongCard.jsx
@@ -10,24 +10,8 @@ export class SongCard extends Component {
 
     render() {
         return (
-            // <div className="card mb-3" style="max-width: 540px;">
-            //     <div className="row no-gutters">
-            //         <div className="col-md-4">
-            //             <img src={placeholder} alt="thumbnail"
-            //             />
-            //         </div>
-            //         <div className="col-md-8">
-            //             <div className="card-body">
-            //                 <h5 className="card-title">Card title</h5>
-            //                 <p className="card-text">This is a wider card with supporting text below as a natural
-            //                     lead-in to additional content. This content is a little bit longer.</p>
-            //                 <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
 
-            <MDBContainer>
+            <MDBContainer fluid className={'pl-0'}>
                 <MDBCard className="align-middle mb-3 text-left" style={{maxWidth: "700px"}}>
                     <div className="row no-gutters">
                         <div className="col-md-3">
