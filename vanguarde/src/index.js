7b14cbcff069d7f9b6a96a737cf752950fa3bb7b
xelav <xelav.rus@gmail.com>
2020-06-12 16:03:41 +0300

    Add redux to package.json

--- vanguarde/src/index.js
+++ vanguarde/src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+const store = createStore()
+
 // TODO: move <link> somewhere more proper
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
